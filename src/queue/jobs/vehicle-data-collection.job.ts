d526925e2d70b79dcf1cf03273af4861bf82943c
Devin Cunningham <dtyler.cunningham@gmail.com>
2022-07-22 07:54:43 -0700

    refactor(sessions): consolidates sessions into one model
    drive, charge, sentry, conditioning, and sleep sessions are now merged into one model named sessions
    with a type key to tell them apart
    
    BREAKING CHANGE: removes all *session models and creates one session model

--- src/queue/jobs/vehicle-data-collection.job.ts
+++ src/queue/jobs/vehicle-data-collection.job.ts
@@ -1,27 +1,31 @@
+/** PACKAGE IMPORTS **/
 import { Job } from 'bullmq';
 import isEmpty from 'lodash/isEmpty';
 import { getDistance } from 'geolib';
+
+/** CONTROLLER LEVEL IMPORTS **/
 import {
   cacheController,
   teslaController,
+  sessionController,
   vehicleController,
-  vehicleDataController,
-  sleepSessionController,
-  driveSessionController,
-  teslaAccountController,
-  chargeSessionController,
-  sentrySessionController,
-  dataCollectorController,
   mapPointController,
-  conditioningSessionController,
+  vehicleDataController,
+  teslaAccountController,
+  dataCollectorController,
 } from '../../controllers';
-import Logger from '../../config/logger';
 import { JobImp, BaseJob } from './job.definition';
-import { buildCacheKey } from '../../utils/formatFuncs';
-import { ISleepSession } from '../../models/sleepSession.model';
-import { IJobData, IVehicleJobPayload } from '../../models/types';
-import { key as sleepSessionKey } from '../../services/sleepSession.service';
+
+/** SERVICE LEVEL IMPORTS **/
 import { loops, key as loopKey, Values } from '../../services/dataCollector.service';
+
+/** TYPE IMPORTS **/
+import { SessionType } from '../../models/session.model';
+import { IJobData, IVehicleJobPayload } from '../../models/types';
+
+/** UTIL and CONFIG IMPORTS **/
+import Logger from '../../config/logger';
+import { buildCacheKey } from '../../utils/formatFuncs';
 
 const logger = Logger('vehicle-data-collection.job');
 
@@ -38,7 +42,7 @@ const handleOuterLoop = async ({ job, teslaAccount, vehicle }: IJobData) => {
   // states ['online', 'offline', 'asleep']
   const { state } = foundVehicle;
 
-  const sleepSessionCacheKey = `${vehicle._id}::${sleepSessionKey}`;
+  const sleepSessionCacheKey = `${vehicle._id}::${SessionType['sleep']}-session`;
   /*
    * if vehicle state from tesla does not match vehicle state
    * stored in db, update the db vehicle state
@@ -46,30 +50,38 @@ const handleOuterLoop = async ({ job, teslaAccount, vehicle }: IJobData) => {
   if (state !== vehicle.state) await vehicleController.updateVehicle(_id, { ...foundVehicle });
 
   switch (state.toLowerCase()) {
-    case 'online':
+    case 'online': {
       // if vehicle is online, switch to inner loop
       await cacheController.deleteCacheByKey(sleepSessionCacheKey);
       await dataCollectorController.setLoop(vehicle._id, loops.inner);
       logger.info(state, { ...baseLogObj });
       break;
+    }
 
     case 'asleep': {
       // if vehicle is offline, continue on outer loop and create or update sleep session
-      const sleepSessionFromCache = (await cacheController.getCache(sleepSessionCacheKey)) as ISleepSession | undefined;
-      const sleepSession = await sleepSessionController.updateSleepSession(sleepSessionFromCache?._id, vehicle);
+      const sleepSessionFromCache = await cacheController.getCache(sleepSessionCacheKey);
+      const sleepSession = await sessionController.upsertSession(
+        sleepSessionFromCache?._id,
+        vehicle.user,
+        vehicle._id,
+        SessionType['sleep']
+      );
 
       await dataCollectorController.setLoopExpiration(vehicle._id);
-      logger.info(state, { ...baseLogObj, session: sleepSession?._id });
+      logger.info(state, { ...baseLogObj, session: sleepSession?._id, sessionType: SessionType['sleep'] });
       break;
     }
-    case 'offline':
+
+    case 'offline': {
       // if vehicle is offline, continue on outer loop
       await dataCollectorController.setLoopExpiration(vehicle._id);
       await cacheController.deleteCacheByKey(sleepSessionCacheKey);
       logger.info(state, { ...baseLogObj });
       break;
+    }
 
-    default:
+    default: {
       /*
        * at the moment tesla states are only 'online', 'asleep', or 'offline'
        * if the default case if hit, tesla has implemented a new state
@@ -77,6 +89,7 @@ const handleOuterLoop = async ({ job, teslaAccount, vehicle }: IJobData) => {
       logger.warn('unhandled state', { ...baseLogObj, state });
       await dataCollectorController.setLoopExpiration(vehicle._id);
       break;
+    }
   }
 };
 
@@ -102,7 +115,7 @@ const handleInnerLoop = async ({ job, teslaAccount, vehicle }: IJobData) => {
    * 2 On, standby or Camp Mode
    * 3 On, charging screen
    * 4 On
-   * 5 On, Big charging screen
+   * 5 On, Big charging screen consolidate
    * 6 On, Ready to unlock
    * 7 Sentry Mode
    * 8 Dog Mode
@@ -130,18 +143,35 @@ const handleInnerLoop = async ({ job, teslaAccount, vehicle }: IJobData) => {
   /******** SENTRY SESSION ********/
   if (sentry_mode) {
     const id = vehicleHasMoved ? null : lastSavedDataPoint?.sentry_session_id;
-    const currentSentrySession = await sentrySessionController.updateSentrySession(id, dataPoint);
+    const currentSentrySession = await sessionController.upsertSession(
+      id,
+      vehicle.user,
+      vehicle._id,
+      SessionType['sentry'],
+      dataPoint
+    );
 
     dataPoint.sentry_session_id = currentSentrySession._id;
 
-    logger.info('sentry session', { ...baseLogObj, is_preconditioning, session: currentSentrySession._id });
+    logger.info('sentry session', {
+      ...baseLogObj,
+      is_preconditioning,
+      session: currentSentrySession._id,
+      sessionType: SessionType['sentry'],
+    });
   }
 
   /******** CONDITIONING SESSION ********/
   /* CLIMATE_KEEPER_MODE states [ 'camp', 'dog', 'off', 'on' ] */
   if (['camp', 'dog', 'on'].includes(climate_keeper_mode) || is_preconditioning) {
     const id = vehicleHasMoved ? null : lastSavedDataPoint?.conditioning_session_id;
-    const currentConditioningSession = await conditioningSessionController.updateConditioningSession(id, dataPoint);
+    const currentConditioningSession = await sessionController.upsertSession(
+      id,
+      vehicle.user,
+      vehicle._id,
+      SessionType['conditioning'],
+      dataPoint
+    );
 
     dataPoint.conditioning_session_id = currentConditioningSession._id;
 
@@ -152,27 +182,42 @@ const handleInnerLoop = async ({ job, teslaAccount, vehicle }: IJobData) => {
       climate_keeper_mode,
       center_display_state,
       session: currentConditioningSession._id,
+      sessionType: SessionType['conditioning'],
     });
   }
 
   /******** DRIVE SESSION ********/
   /* SHIFT_STATE states [ null, P, R, N, D ] */
   if (['p', 'r', 'n', 'd'].includes(shift_state_lower as string)) {
-    const currentDriveSession = await driveSessionController.updateDriveSession(
+    const currentDriveSession = await sessionController.upsertSession(
       lastSavedDataPoint?.drive_session_id,
+      vehicle.user,
+      vehicle._id,
+      SessionType['drive'],
       dataPoint
     );
 
     dataPoint.drive_session_id = currentDriveSession._id;
 
-    logger.info('drive session', { ...baseLogObj, shift_state, session: currentDriveSession._id });
+    logger.info('drive session', {
+      ...baseLogObj,
+      shift_state,
+      session: currentDriveSession._id,
+      sessionType: SessionType['drive'],
+    });
   }
 
   /******** CHARGE SESSION ********/
   /* CHARGE_STATE states [ Charging, Complete, Disconnected, NoPower, Starting, Stopped ] */
   if (['charging', 'complete', 'nopower', 'starting', 'stopped'].includes(charging_state_lower)) {
     const id = vehicleHasMoved ? null : lastSavedDataPoint?.charge_session_id;
-    const currentChargeSession = await chargeSessionController.updateChargeSession(id, dataPoint);
+    const currentChargeSession = await sessionController.upsertSession(
+      id,
+      vehicle.user,
+      vehicle._id,
+      SessionType['charge'],
+      dataPoint
+    );
 
     dataPoint.charge_session_id = currentChargeSession._id;
 
@@ -180,11 +225,13 @@ const handleInnerLoop = async ({ job, teslaAccount, vehicle }: IJobData) => {
       ...baseLogObj,
       charging_state,
       session: currentChargeSession._id,
+      sessionType: SessionType['charge'],
     });
   }
 
   await vehicleDataController.saveVehicleData(dataPoint);
 };
+/** END LOOPS **/
 
 /** JOB **/
 export class VehicleDataCollection extends BaseJob implements JobImp {
@@ -196,9 +243,9 @@ export class VehicleDataCollection extends BaseJob implements JobImp {
     const _id = this.payload.vehicle;
     const vehicle = await vehicleController.getVehicle(_id);
     if (!vehicle || isEmpty(vehicle)) throw new Error('Missing vehicle');
-    const { teslaAccount: ta, id_s } = vehicle;
+    const { teslaAccount: taID, id_s } = vehicle;
 
-    const teslaAccount = await teslaAccountController.getTeslaAccount(ta, _id);
+    const teslaAccount = await teslaAccountController.getTeslaAccount(taID, _id);
 
     // handle error cases
     if (!teslaAccount || isEmpty(teslaAccount)) throw new Error('Missing tesla account');
@@ -206,7 +253,7 @@ export class VehicleDataCollection extends BaseJob implements JobImp {
       throw new Error('Missing or bad access token');
     if (!id_s || typeof id_s !== 'string') throw new Error('Missing or bad id_s');
 
-    const loopCacheKey = buildCacheKey(_id, '', loopKey);
+    const loopCacheKey = buildCacheKey(_id, loopKey);
 
     // get the current loop from cache; Defaults to outer loop
     const loop = (await dataCollectorController.getLoop(vehicle._id)) as Values;
@@ -228,6 +275,7 @@ export class VehicleDataCollection extends BaseJob implements JobImp {
     }
   };
 
+  /** HANDLE ERRORS **/
   // all thrown errors are handled here
   failed = async (job: Job): Promise<void> => {
     const _id = this.payload.vehicle;
